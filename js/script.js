e2345158701c4cb28ff8a941c6a80d77e8379816
Arrow Brave <arrowbrave@users.noreply.github.com>
2023-01-25 13:32:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -105,6 +105,7 @@ const saveAttendanceRecords = function() {
     data.append('time_attended', time_attended);
     data.append('platform', platform);
     data.append('youth', youth);
+    console.log(formData(data));
 
     fetch(googleMacroURL, {
         method: 'POST',
